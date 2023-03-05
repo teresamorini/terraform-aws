c19051b2051d342e9e2e44fb7e18064d49b4b0b5
teresamorini <teresa.morini91@gmail.com>
2023-03-05 18:55:14 +0100

    vpc - subnet config

--- main.tf
+++ main.tf
@@ -2,12 +2,23 @@ provider "aws" {
     region= "eu-west-3"
 }
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = "10.0.0.0/16" # private ip adresses range
+variable vpc_cidr_blocks {}
+variable subnet_cidr_blocks {}
+variable avail_zone {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_blocks # private ip adresses range
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = "10.0.10.0/24"
-    availability_zone = "eu-west-3a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_blocks
+    availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-subnet-1"
+    }
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_cidr_blocks = "10.0.0.0/16"
+subnet_cidr_blocks = "10.0.10.0/24"
+avail_zone = "eu-west-3a"
+env_prefix = "dev"
