e3f863081b1455148d8224afe9cffca3947ccdce
teresamorini <teresa.morini91@gmail.com>
2023-03-05 11:02:01 +0100

    create vpc and subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
     region= "eu-west-3"
 }
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16" # private ip adresses range
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "eu-west-3a"
+}
